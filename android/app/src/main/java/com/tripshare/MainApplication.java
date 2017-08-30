4d87e2d804c6550b2cb55436c5a9f784180e0b6f
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-08-30 14:17:00 -0600

    Migrated working files

--- android/app/src/main/java/com/tripshare/MainApplication.java
+++ android/app/src/main/java/com/tripshare/MainApplication.java
@@ -3,6 +3,9 @@ package com.tripshare;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.anyline.reactnative.AnylinePackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new AnylinePackage(),
+            new VectorIconsPackage(),
+            new RCTCameraPackage()
       );
     }
   };
