bc5f9b4ab0ed9bbc1ebac277f40089d7c0cad9ae
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-08-31 09:30:52 -0600

    basic styling for total

--- components/Total.js
+++ components/Total.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, Alert } from 'react-native';
 import { NavigationActions } from 'react-navigation';
 import { List, ListItem } from 'react-native-elements';
 
@@ -38,18 +38,55 @@ export default class Total extends React.Component {
     });
   }
 
+  pay = (e) => {
+    const text = e.amount_owed < 0 ? `Did ${e.name} receive all money owed?` : `Did ${e.name} fully pay?`;
+    Alert.alert(
+      text,
+      null,
+      [
+        {text: 'No', onPress: () => this.update(e, false), style: 'cancel'},
+        {text: 'Yes', onPress: () => this.update(e, true)},
+      ],
+      { cancelable: false }
+    );
+  }
+
+  update = (e, bool) => {
+    if (e.paid === bool) return;
+    e.paid = !e.paid;
+    const trip_users = [...this.state.trip_users];
+    for (let i = 0; i < trip_users.length; i++) {
+      if (trip_users[i].id === e.id) {
+        trip_users[i] = e;
+        break;
+      }
+    }
+    this.setState({ trip_users });
+    console.log(e.paid);
+    fetch(`https://split-trip.herokuapp.com/users/${e.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({paid: e.paid})
+    }).then((res) => {
+      console.log(res);
+    });
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <List containerStyle={{marginBottom: 20, marginTop: 0}}>
-          <ListItem hideChevron title={`Trip total: $${this.state.totals.total}`} rightTitle={`Individual cost: $${this.state.totals.individualCost}`} rightTitleStyle={{color: 'black', fontSize: 15}} titleStyle={{color: 'black', fontSize: 15}}/>
+          <ListItem hideChevron title={`Trip cost: $${this.state.totals.total}`} rightTitle={`Individual cost: $${this.state.totals.individualCost}`} rightTitleStyle={{color: 'black', fontSize: 15}} titleStyle={{color: 'black', fontSize: 15}}/>
           {
             this.state.trip_users.sort((a, b) => (a.name > b.name) ? 1 : -1).map(e => {
               const color = Number(e.amount_owed) > 0 ? 'red' : 'green';
               const amount = Number(e.amount_owed) > 0 ? e.amount_owed : e.amount_owed.slice(1);
               return (
-                <ListItem roundAvatar avatar={{ uri: e.image_url }} key={e.id} title={e.name} rightTitle={amount} rightTitleStyle={{color}} hideChevron subtitle={e.paid ? 'paid' : null} />
+                <ListItem roundAvatar avatar={{ uri: e.image_url }} key={e.id} title={e.name} rightTitle={amount} rightTitleStyle={{color}} hideChevron subtitle={e.paid ? 'paid' : null} onPress={() => this.pay(e)} subtitleStyle={{fontSize: 12}}/>
               );
             })
           }
